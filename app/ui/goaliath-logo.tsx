8fa153be585aac6d6d84e10d9c16d775345d9cb7
deanrid86 <deanrid86@gmail.com>
2023-11-28 13:07:33 +0000

    Initial commit

---
+++ app/ui/goaliath-logo.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image';
+
+export default function Goaliath_Logo () {
+return (
+    <div> 
+        <Image className="h-20 w-20 rotate-[15deg]"
+        src= '/Goaliath_Logo.png'
+        height = {240}
+        width = {240}
+        alt = 'The Goaliath Logo'/>
+        <p className="text-[44px]">Goaliath</p>
+    </div>
+    
+       
+    
+);
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,6 +17,11 @@ const config: Config = {
           500: '#0070F3',
           600: '#2F6FEB',
         },
+        greenblue: {
+          400: 'rgb(0, 100, 120)',
+          500: 'rgb(0, 100, 100)',
+          600: 'rgb(0, 100, 80)',
+        } 
       },
     },
     keyframes: {
